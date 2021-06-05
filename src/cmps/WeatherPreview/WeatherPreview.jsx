a6a98fc40a3cfffccac60df881ed3022c2b5abf3
Yulia Alexeev <july90a@gmail.com>
2021-06-05 14:44:44 +0300

    added date convertion function

--- src/cmps/WeatherPreview/WeatherPreview.jsx
+++ src/cmps/WeatherPreview/WeatherPreview.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './WeatherPreview.scss';
 
 export function WeatherPreview({ weather }) {
-    return (
-        <li className="weather-preview">
-            {weather.Date}
-            <img
-                src={`https://developer.accuweather.com/sites/default/files/0${weather.Day.Icon}-s.png`}
-            />
-            <h3>{weather.Day.IconPhrase}</h3>
-        </li>
-    );
+  function convertDate(timeStemp) {
+    var date = new Date();
+    date.setTime(timeStemp * 1000);
+    return date.toUTCString().substring(0,11);
+  }
+
+  return (
+    <li className="weather-preview">
+      {convertDate(weather.EpochDate)}
+      <img
+          src={`https://developer.accuweather.com/sites/default/files/0${weather.Day.Icon}-s.png`}
+      />
+      <h3>{weather.Day.IconPhrase}</h3>
+    </li>
+  );
 }
