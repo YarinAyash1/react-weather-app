0c6942aa1756fd80c74c4baeca50a7fc26799900
Yulia Alexeev <july90a@gmail.com>
2021-06-11 16:13:52 +0300

    added weather icon converter func & css

--- src/cmps/WeatherPreview/WeatherPreview.jsx
+++ src/cmps/WeatherPreview/WeatherPreview.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import './WeatherPreview.scss';
 
 export function WeatherPreview({ weather }) {
-  function convertDate(timeStemp) {
+  function dateConverter(timeStemp) {
     var date = new Date();
     date.setTime(timeStemp * 1000);
     return date.toUTCString().substring(0,11);
   }
 
+  function weatherIconConverter(iconNum){
+    return iconNum < 10?  '0' + iconNum : iconNum;
+  }
+
   return (
-    <li className="weather-preview">
-      {convertDate(weather.EpochDate)}
+    <li className="weather-preview weather-preview-day">
+      {dateConverter(weather.EpochDate)}
       <img
-          src={`https://developer.accuweather.com/sites/default/files/0${weather.Day.Icon}-s.png`}
+          src={`https://developer.accuweather.com/sites/default/files/${weatherIconConverter(weather.Day.Icon)}-s.png`}
       />
       <h3>{weather.Day.IconPhrase}</h3>
+      <span className="weather-preview-temperature">{weather.Temperature.Maximum.Value} &#8451; | {weather.Temperature.Minimum.Value} &#8451;</span>
+      <div className="weather-preview weather-preview-night">
+        <img
+            src={`https://developer.accuweather.com/sites/default/files/${weatherIconConverter(weather.Night.Icon)}-s.png`}
+        />
+        <h3>{weather.Night.IconPhrase}</h3>
+      </div>
     </li>
   );
 }
