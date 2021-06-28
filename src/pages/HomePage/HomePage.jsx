dee792d03fea8a1e20503dff5b5f322d410ecdad
Yulia Alexeev <july90a@gmail.com>
2021-06-28 20:41:19 +0300

    added redux & redux thunk

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,70 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { CurrentWeather } from '../../cmps/CurrentWeather/CurrentWeather';
 import { WeatherList } from '../../cmps/WeatherList/WeatherList';
 import { WeatherSearch } from '../../cmps/WeatherSearch/WeatherSearch';
 import { CityPreview } from '../../cmps/CityPreview/CityPreview';
 import { weatherService } from '../../services/weatherService';
 import { ToastContainer, toast } from 'react-toastify';
+import { loadWeather, loadCurrentWeather, loadCity } from '../../store/actions/weatherActions';
 import 'react-toastify/dist/ReactToastify.css';
-
 import './HomePage.scss';
 
-export default class HomePage extends React.Component {
-  state = {
-      weather: null,
-      currentWeather: null,
-      city: null
-  };
-
+class _HomePage extends React.Component {
   componentDidMount() {
-      this.loadWeather();
-      this.loadCurrentWeather();
-      this.loadCity();
+    this.props.loadWeather();
+    this.props.loadCurrentWeather();
+    this.props.loadCity();
   }
 
   handleClick = (addToFavorites) => {
     console.log('this is:', addToFavorites);
     weatherService.favoritesList(addToFavorites);
     toast.success("Added succsesfully to favorites!");
-
-  }
-
-  // notify = () => toast("Added succsesfully to favorites!");
-
-  
-  async loadWeather() {
-      const weather = await weatherService.getWeather();
-      this.setState({ weather });
-  }
-
-  async loadCity() {
-    const city = await weatherService.getCity();
-    this.setState({ city });
-  }
-
-  async loadCurrentWeather() {
-      const currentWeather = await weatherService.getCurrentWeather();
-      this.setState({ currentWeather });
   }
 
   render() {
-    const weather = this.state.weather;
-    const currentWeather = this.state.currentWeather;
-    const city = this.state.city;
+    const { weather, currentWeather, city } = this.props;
     return (
-        <>
-          <div className="home-page container">
-            <div class="city">
-             {city && <CityPreview city={city}/>}
-            </div>
-
-            <button className="favoriters-btn" onClick={() => this.handleClick(currentWeather)} >Add to favorites &#x2764;</button>
-            <CurrentWeather currentWeather={currentWeather}  />
-            <WeatherSearch />
-            {weather && <WeatherList weathers={weather} />}
-            <ToastContainer />
-          </div>
-        </>
+      <>
+        <div className="home-page container">
+          {city && <CityPreview city={city}/>}
+          <button className="favoriters-btn" onClick={() => this.handleClick(currentWeather)} >Add to favorites &#x2764;</button>
+          <CurrentWeather currentWeather={ currentWeather }  />
+          <WeatherSearch />
+          {weather && <WeatherList weathers={ weather } />}
+          <ToastContainer />
+        </div>
+      </>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    weather: state.weatherReducer.weather,
+    currentWeather: state.weatherReducer.currentWeather,
+    city: state.weatherReducer.city,
+  }
+}
+
+const mapDispatchToProps = {
+  loadWeather,
+  loadCurrentWeather,
+  loadCity
+}
+
+export const HomePage = connect(mapStateToProps, mapDispatchToProps)(_HomePage)
