6929ec776e66ac979a44a509f5ae1cd3f9184ee8
Yulia Alexeev <july90a@gmail.com>
2021-07-13 14:08:35 +0300

    added react hooks & general changes in the cmp

--- src/cmps/WeatherSearch/WeatherSearch.jsx
+++ src/cmps/WeatherSearch/WeatherSearch.jsx
@@ -1,55 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useRef } from 'react';
 import { weatherService } from '../../services/weatherService';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './WeatherSearch.scss';
 
-export class WeatherSearch extends Component {
-  state = {
-    autocomplete: '',
-  };
+export function WeatherSearch(){
+  const [autoComplete, setAutoComplete] = useState(null);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
-  onChangeHandler = (ev) => {
-    console.log(ev.target)
+  useEffect(() => {
+    const loadAutoComplete = async () => {
+      const res = await weatherService.getLocationAutoComplete();
+      setAutoComplete(res);
+    } 
+    loadAutoComplete();
+  }, [])
+  
+  const onChangeHandler = async (text) =>{
     var letters = /^[a-zA-Z\s]*$/;
-    if(!ev.target.value.match(letters)){
-      toast.warn("It must be only English letters only");
+    if(!text.match(letters)){
+      toast.warn('It must be English letters only');
       return;
     } else {
-      console.log('search,', ev.target.value);
-      weatherService.getLocationAutoComplete(ev.target.value);
+      let matches = [];
+      if(text.length > 0){
+        matches = autoComplete.filter(acmplt => {
+          const regex = new RegExp(`${text}`, 'gi');
+          return acmplt.LocalizedName.match(regex);
+         })
+      }
+      setSuggestions(matches);
+      setText(text); 
     }
-  };
-
-  componentDidMount() {
-    this.loadAutoComplete();
   }
 
-  async loadAutoComplete() {
-    const autocomplete = await weatherService.getLocationAutoComplete();
-    console.log('res auto complete', autocomplete);
-    this.setState({ autocomplete });
+  const onSelectedCityHandler = (text) => {
+    setText(text);
+    weatherService.getLocationAutoComplete(text)
+    setSuggestions([]);
   }
 
-  render() {
-    const autocomplete = this.state.autocomplete;
-    return (
-      <form className="weather-search">
-        <input
-          type="text"
-          className="weather-search-input"
-          onChange={this.onChangeHandler}
-          placeholder="Type location"
-          list="locations"
-        />
-        <datalist id="locations">
-          <ul className="feature-list clean-list">
-            {autocomplete && autocomplete.map((city) => (
-              <option key={city.Key} value={city.LocalizedName }/>
-            ))}
-          </ul>
-        </datalist>
-      </form>
-    );
-  }
+  return (  
+    <form className="weather-search">
+      <input
+        type="text"
+        className="weather-search-input"
+        onChange={e => onChangeHandler(e.target.value)}
+        value={text}
+        placeholder="Type city"
+      />
+      <div className="suggestion-list">
+        {suggestions && suggestions.map((suggestion, i) => 
+          <div key={i} className="suggestion-list-item" onClick={() => onSelectedCityHandler(suggestion.LocalizedName)}>{suggestion.LocalizedName}</div>
+        )}
+      </div>
+    </form>
+  );
 }
